3c91be441d26b9cbae2e362d01007a2dcd53f0a0
jmolinaing <jmolina.ing@gmail.com>
2026-01-21 20:22:29 -0300

    20260121

--- gestion_cobranza/20260115_req17333_Mantenedor_deudores_part2/doc/bd_isapre/SQLQuery1.sql
+++ gestion_cobranza/20260115_req17333_Mantenedor_deudores_part2/doc/bd_isapre/SQLQuery1.sql
@@ -7,6 +7,42 @@ select * from fun with (nolock) where fun_tipo like '%1%' and con_folio = 157333
 
 insert Gescob_ejecutivo_cuestionado values ('  89455146', 'obs')
 
+select * from dbo.COTIZACION_PAGADA cp with (nolock)
+
+select max(ppc_fecha_pago) from dbo.COTIZACION_PAGADA cp with (nolock) where cp.epa_rut = ' 763717674'
+select max(ppc_fecha_pago) from dbo.planilla pp with (nolock) where pp.epa_rut = ' 763717674'
+
+SELECT MAX(fecha_pago) AS max_fecha_pago
+FROM (
+    SELECT cp.ppc_fecha_pago AS fecha_pago
+    FROM dbo.COTIZACION_PAGADA cp WITH (NOLOCK) 
+    WHERE cp.epa_rut = ' 763717674'
+    
+    UNION ALL
+    
+    SELECT pp.ppc_fecha_pago
+    FROM dbo.planilla pp WITH (NOLOCK) 
+    WHERE pp.epa_rut = ' 763717674'
+) t
+
+
+(
+SELECT t1.epa_rut, MAX(t1.fecha_pago) AS ppc_fecha_pago
+FROM (
+    SELECT  epa_rut, max(cp.ppc_fecha_pago) AS fecha_pago
+    FROM dbo.COTIZACION_PAGADA cp WITH (NOLOCK)
+    group by epa_rut
+    --WHERE cp.epa_rut = mdeu.ddr_rut
+    
+    UNION ALL
+    
+    SELECT epa_rut, max(pp.ppc_fecha_pago)
+    FROM dbo.planilla pp WITH (NOLOCK) 
+    group by epa_rut
+    --WHERE pp.epa_rut = mdeu.ddr_rut
+) t1
+group by t1.epa_rut
+) ufp
 
 select *
 from dbo.contrato c with (nolock)   
@@ -37,3 +73,25 @@ and c.cot_rut
         )   and    
          (f2.age_codigo = @age or coalesce(@age,0) = 0) and   
          (f2.eje_rut = @rut_ejecutivo or coalesce(@rut_ejecutivo,'') = '' )  
+
+
+
+         select * from parametros where par_codigo='ucc'
+
+
+
+
+
+
+--Contable cerrado
+	select par_finvig from parametros where par_codigo='ucc'
+
+    select dateadd(m, -60,  par_finvig)  from parametros where par_codigo='ucc'
+		
+--Deuda Total
+	SELECT DEC_RUT, DEC_PERIODO, COT_RUT, DEC_PACTADO - DEC_PAGADO AS DEUDA FROM DEUDA_COTIZANTE
+
+--Deuda con RJ
+	SELECT DDR_RUT, RJD_PERIODO, COT_RUT FROM RESOLUCION_JUDICIAL R
+			JOIN RJUD_DEUDA RD ON R.REJ_FOLIO=RD.REJ_FOLIO
+
---
+++ gestion_cobranza/20260115_req17333_Mantenedor_deudores_part2/doc/bd_isapre/spu_ges_cob_cju_ges_deuda_v5.sql
@@ -0,0 +1,176 @@
+  
+  
+  
+/* =======================================================================================*/  
+/*  Tipo de Objeto     : Procedimiento Almacenado            */  
+/*  Nombre Objeto      : spu_ges_cob_cju_ges_deuda            */  
+/*  Parmetros          : @p_cod_cob : codigo de cobrador judicial, ->0 para anular filtro */  
+/*        : @p_ind_rjud: indicador de exclusión de deudores sin resolución   */  
+/*           judicial, ->S Excluye ->N no excluye      */  
+/*  Sistema            : PB10 Gestion Cobranza             */  
+/*  Destino            : BD ISAPRE                */  
+/*  Creado por         : Proveedor Externo Aligare - Pablo Melo         */  
+/*  Fecha Creacin      : 12-04-2022                */  
+/*  Descripcion        : Obtiene deudores en cobranza judicial a quienes se puede    */  
+/*       generar resoluciones nuevas o complementarias.       */  
+/* =======================================================================================*/  
+  
+-- spu_ges_cob_cju_ges_deuda 27,S  
+-- spu_ges_cob_cju_ges_deuda 27,N  
+-- spu_ges_cob_cju_ges_deuda 0,S  
+-- spu_ges_cob_cju_ges_deuda 0,N  
+
+--execute spu_ges_cob_cju_ges_deuda null, 'S', 's', 'N', 's'
+  
+alter PROCEDURE [dbo].[spu_ges_cob_cju_ges_deuda] @p_cod_cob numeric(4), @p_ind_rjud char(1)  , @excluir_mediatica char(1) = null, @excluir_quiebra char(1) = null, @excluir_venta char(1) = null
+AS  
+BEGIN   
+ SET NOCOUNT ON;  
+   
+select mcob.cob_codigo  
+   ,0 as pcheck  
+   ,mdeu.ddr_rut as deudor_rut  
+   ,upper(rtrim(ltrim(mdeu.ddr_nombre))) as deudor_nombre  
+   ,odasgper.DEC_PERIODO  
+   ,(select sum (dec_pactado - dec_pagado) from DEUDA_COTIZANTE dc WITH (NOLOCK) where dc.DEC_RUT=odasg.DDR_RUT and dc.COT_RUT=odasgper.COT_RUT and dc.DEC_PERIODO= odasgper.DEC_PERIODO) as deuda  
+   ,(select sum(DD.DDT_SALDO) from DNP WITH (NOLOCK) join  DNP_DETALLE dd WITH (NOLOCK) on dd.PPC_FOLIO=dnp.PPC_FOLIO where dnp.epa_rut =odasg.DDR_RUT and cot_rut =odasgper.COT_RUT and dd.PPC_PERIODO=odasgper.DEC_PERIODO) as dnp  
+   ,case when COALESCE(odasgper.dec_tipo_deuda,'NP') ='NP' then COALESCE(odasgper.dec_deuda,0) else 0 end as deuda_monto_np  
+   ,case when COALESCE(odasgper.dec_tipo_deuda,'NP') ='DNP' then COALESCE(odasgper.dec_deuda,0) else 0 end as deuda_monto_dnp  
+   ,odasg.deu_correl as deu_correl  
+   ,upper(rtrim(ltrim(mcob.cob_nombre))) as cob_nom  
+   ,( select case when count(rjud.rej_folio)>0 then 1 else 0 end   
+        from resolucion_judicial rjud WITH (NOLOCK)  
+       where rjud.ddr_rut = mdeu.ddr_rut  
+     ) as cob_rju_vig
+	, mdeu.ddr_mediatica ddr_mediatica
+	, mdeu.ddr_quiebra ddr_quiebra
+	  , CASE 
+    WHEN EXISTS (
+        SELECT 1
+        FROM dbo.contrato c WITH (NOLOCK)   
+        JOIN dbo.fun f WITH (NOLOCK) 
+            ON f.con_folio = c.con_folio    
+        JOIN dbo.Gescob_ejecutivo_cuestionado g WITH (NOLOCK)
+            ON g.eje_rut = f.eje_rut
+        WHERE f.fun_tipo LIKE '%1%'
+            AND c.con_ultimo = 'S'
+            AND c.cot_rut = mdeu.ddr_rut
+    ) THEN 'X' 
+    ELSE '' 
+END AS venta_cuestionada
+  --	, (select max(ppc_fecha_pago) from dbo.planilla pp with (nolock) where pp.epa_rut = mdeu.ddr_rut  ) as ult_fecha_pago
+  , (SELECT MAX(fecha_pago) AS max_fecha_pago
+FROM (
+    SELECT cp.ppc_fecha_pago AS fecha_pago
+    FROM dbo.COTIZACION_PAGADA cp WITH (NOLOCK) 
+    WHERE cp.epa_rut = mdeu.ddr_rut
+    
+    UNION ALL
+    
+    SELECT pp.ppc_fecha_pago
+    FROM dbo.planilla pp WITH (NOLOCK) 
+    WHERE pp.epa_rut = mdeu.ddr_rut
+) t
+) as ult_fecha_pago
+  into #deuda_pre  
+  from cobrador as mcob WITH (NOLOCK)  
+   join deudor_asignado as odasg WITH (NOLOCK) on odasg.cob_codigo = mcob.cob_codigo and odasg.deu_asig_desde < getdate() and ( odasg.deu_asig_hasta >= getdate() or odasg.deu_asig_hasta is null)  
+   join deuasig_periodo as odasgper WITH (NOLOCK) on odasgper.deu_correl = odasg.deu_correl  
+   join deudor as mdeu WITH (NOLOCK) on mdeu.ddr_rut = odasg.ddr_rut  
+  where mcob.cob_judicial ='S'  
+   and (mcob.cob_codigo=@p_cod_cob or  @p_cod_cob is null)  
+   and (@p_ind_rjud ='N' or not exists (  
+      select 1  
+     from DEUDA_COTIZANTE dc WITH (NOLOCK) where dc.DEC_RUT=odasg.DDR_RUT and dc.COT_RUT=odasgper.COT_RUT and dc.DEC_PERIODO= odasgper.DEC_PERIODO and DEC_NRORESOL > 0  
+      ))  
+    and ( (@excluir_mediatica = 'S' and (mdeu.ddr_mediatica = 'N' or mdeu.ddr_mediatica is null))
+            or @excluir_mediatica = 'N' or @excluir_mediatica is null
+            )
+    and ( (@excluir_quiebra = 'S' and (mdeu.ddr_quiebra = 'N' or mdeu.ddr_quiebra is null )   )
+            or @excluir_quiebra = 'N' or @excluir_quiebra is null
+            )
+
+
+
+	select *
+	into #deuda
+	from #deuda_pre 
+	where ( (@excluir_venta = 'S' and (venta_cuestionada = '' or venta_cuestionada is null )   )
+            or @excluir_venta = 'N' or @excluir_venta is null
+            )
+  
+  update #deuda set deuda=0 where deuda <0 or deuda is null  
+  update #deuda set dnp=0 where dnp <0 or dnp is null  
+  
+  select   
+  cob_codigo, pcheck, deudor_rut, deudor_nombre, DEC_PERIODO, 'NP' as tipo_deuda, (deuda - dnp) as deuda , deu_correl, cob_nom, cob_rju_vig  
+ , ddr_mediatica, ddr_quiebra, venta_cuestionada, ult_fecha_pago
+  into #respuesta  
+  from #deuda where deuda > dnp  
+  
+  union all  
+  
+  select   
+  cob_codigo, pcheck, deudor_rut, deudor_nombre, DEC_PERIODO, 'DNP' as tipo_deuda, dnp as deuda , deu_correl, cob_nom, cob_rju_vig 
+   , ddr_mediatica, ddr_quiebra , venta_cuestionada, ult_fecha_pago
+  from #deuda where dnp > 0  
+  
+  
+  select   
+   cob_codigo,   
+   pcheck,   
+   deudor_rut,   
+   deudor_nombre,   
+   count (distinct case when tipo_deuda ='NP' then DEC_PERIODO else null end) as deuda_tipo_np_ind,  
+   count (distinct case when tipo_deuda ='DNP' then DEC_PERIODO else null end) as deuda_tipo_dnp_ind,  
+   sum(case when tipo_deuda ='NP' then deuda else 0 end) as deuda_monto_np,  
+   sum(case when tipo_deuda ='DNP' then deuda else 0 end) as deuda_monto_dnp,  
+   deu_correl,   
+   cob_nom,   
+   cob_rju_vig  
+  , case when ddr_mediatica = 'S' then 'X' else '' end as ddr_mediatica
+  , case when ddr_quiebra = 'S' then 'X' else '' end as ddr_quiebra
+  --, (select case when count(*) > 0 then 'X' else '' end
+  --      from dbo.contrato c with (nolock)   
+  --      join dbo.fun f with (nolock) 
+  --          on f.con_folio= c.con_folio    
+  --               --left join cotizante  (nolock) on cotizante.cot_rut=c.cot_rut  
+  --               --left join ejecutivo e (nolock) on e.eje_rut=f2.eje_rut  
+  --      join dbo.Gescob_ejecutivo_cuestionado g with (nolock)
+  --             on g.eje_rut = f.eje_rut
+  --      where f.fun_tipo like ( '%1%')
+  --      and c.con_ultimo = 'S'
+  --      and c.cot_rut = deudor_rut
+  --      ) as venta_cuestionada
+--  , CASE 
+--    WHEN EXISTS (
+--        SELECT 1
+--        FROM dbo.contrato c WITH (NOLOCK)   
+--        JOIN dbo.fun f WITH (NOLOCK) 
+--            ON f.con_folio = c.con_folio    
+--        JOIN dbo.Gescob_ejecutivo_cuestionado g WITH (NOLOCK)
+--            ON g.eje_rut = f.eje_rut
+--        WHERE f.fun_tipo LIKE '%1%'
+--            AND c.con_ultimo = 'S'
+--            AND c.cot_rut = deudor_rut
+--    ) THEN 'X' 
+--    ELSE '' 
+--END AS venta_cuestionada
+, venta_cuestionada
+  , ult_fecha_pago
+  , cast(null as numeric(15)) as deuda5anos
+  from #respuesta  
+  group by cob_codigo,pcheck, deudor_rut, deudor_nombre, deu_correl , cob_nom, cob_rju_vig  
+    , ddr_quiebra
+  , ddr_mediatica
+  , venta_cuestionada
+  , ult_fecha_pago
+  --having  (@p_ind_rjud ='N' or not exists (  
+  --    select 1  
+  --   from resolucion_judicial rjud WITH (NOLOCK)  
+  --    where rjud.deu_correl = #respuesta.deu_correl  
+  --    ) )  
+  order by 4 asc  
+    
+    
+END  
